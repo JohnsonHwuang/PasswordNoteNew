2ad563600273cc28601f94e14fd7f2a418351368
郑鸿川 <zhenghongchuan@icloud.com>
2017-07-21 01:55:10 +0800

    阴影终于搞定了...

---
+++ PasswordNote/RootViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RootViewController.swift
+//  PasswordNote
+//
+//  Created by 郑鸿川 on 2017/7/19.
+//  Copyright © 2017年 郑鸿川. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PasswordNote/Views/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  PasswordNote
+//
+//  Created by 郑鸿川 on 2017/7/20.
+//  Copyright © 2017年 郑鸿川. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
